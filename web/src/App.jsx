4d76e2688a214cb7a7e9bd3bd56e2a19e33c88db
Aman <amansharma12607@gmail.com>
2024-09-16 01:12:00 +0530

    added a few structures

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Box, CssBaseline } from '@mui/material';
+import Header from './components/Header';
+import HomePage from './components/HomePage';
+import Error404 from './components/Error404';
+import Footer from './components/Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+        <Header />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/404" element={<Error404 />} />
+            <Route path="*" element={<Navigate to="/404" replace />} />
+          </Routes>
+        <Footer />
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
