0d47e3523467522b3c8af5db633da22e21749acd
Aman <amansharma12607@gmail.com>
2024-09-16 23:54:05 +0530

    jeewan finish

--- server/src/assets/models/Asset.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('../../../services/mongoose');
-
-const assetSchema = new mongoose.Schema({
- 
-});
-
-const Asset = mongoose.model('Assets', assetSchema);
-module.exports = Asset;
--- server/src/assets/routes.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const catalogueController = require('./controllers/catalogueController');
-const auth = require('../../middlewares/auth');
-
-module.exports = router;
